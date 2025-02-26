7e3bdf97986d470011984d96f9939e2ba7458e01
Hikmet Can Çubukçu <40365975+hikmetc@users.noreply.github.com>
2025-02-27 00:12:56 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="Lab Error Finder", page_icon="📖🧪🔍")
+st.set_page_config(page_title="Lab Error Finder", page_icon="🔍")
 import pandas as pd
 
 # --- Custom CSS for an enhanced aesthetic interface with a unified card layout ---
